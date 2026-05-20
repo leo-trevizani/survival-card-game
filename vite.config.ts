7a7689ed3e155889464124c78b3f9921ba9634da
Leo Trevizani <65292051+leo-trevizani@users.noreply.github.com>
2026-05-20 15:59:35 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import {defineConfig} from 'vite';
 export default defineConfig(() => {
   return {
     plugins: [react(), tailwindcss()],
+    base: '/survival-card-game/',
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
